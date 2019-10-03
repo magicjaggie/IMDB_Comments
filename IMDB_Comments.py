3a678ae15435a504a814928c0c644f6405eca2c1
magicjaggie <cle.morgane@gmail.com>
2019-10-03 23:02:57 +0200

    Merge branch 'master' of https://github.com/magicjaggie/IMDB_Comments
     Conflicts:
    	IMDB_Comments.py

--- IMDB_Comments.py
+++ IMDB_Comments.py
@@ -5,10 +5,10 @@ import os
 # Load the dataset
 path = "/Users/morgane/Desktop/DL/aclImdb/"
 
-train_pos = ''
-train_neg = ''
-test_pos = ''
-test_neg = ''
+train_pos = []
+train_neg = []
+test_pos = []
+test_neg = []
 comment = ''
 
 for elt in os.listdir(path + "train/pos"):
@@ -18,10 +18,10 @@ for elt in os.listdir(path + "train/pos"):
     train_pos = train_pos + comment
 
 for elt2 in os.listdir(path + "‎train/neg"):
-    file2 = open(elt2,'r')
-    comment2 = file2.read()
-    file2.close()
-    train_neg = train_neg + comment2
+    file = open(elt2,'r')
+    comment = file.read()
+    file.close()
+    train_neg = train_neg + comment
 
 for elt3 in os.listdir(path + "‎test/pos"):
     file = open(elt3,'r')
