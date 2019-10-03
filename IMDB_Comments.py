1314739ab2dadb0c805a11d9d5ff4de0bf1f3c9d
carlivoix <charlesvivoix@gmail.com>
2019-10-03 21:43:22 +0200

    loads the dataset

--- IMDB_Comments.py
+++ IMDB_Comments.py
@@ -1,2 +1,13 @@
 # Classification of the comments concerning a movie
 
+import keras
+
+(x_train, y_train), (x_test, y_test) = keras.datasets.imdb.load_data(path="imdb.npz",
+                                                                     num_words=None,
+                                                                     skip_top=0,
+                                                                     maxlen=100,
+                                                                     seed=113,
+                                                                     start_char=1,
+                                                                     oov_char=2,
+                                                                     index_from=3)
+
