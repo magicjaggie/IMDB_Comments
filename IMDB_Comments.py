c0f39624da2f8f376085b2c712f765e9107a8c63
magicjaggie <cle.morgane@gmail.com>
2019-10-05 19:09:58 +0200

    Sequential model

--- IMDB_Comments.py
+++ IMDB_Comments.py
@@ -6,6 +6,28 @@ from keras.models import Sequential
 # Load the dataset
 path = "/Users/morgane/Desktop/DL/aclImdb/"
 
+def delPonct(texte):
+    texte.replace(",","").replace("!","").replace(".","").replace("?","").replace(";","").replace('"','').replace("(","").replace(")","")
+    return(texte)
+
+def frqWords(listWords):
+    v = 0
+    d = dict()
+    for i1 in range(0,len(listWords)):
+        for i2 in range(0,len(listWords)):
+            if listWords[i1] == listWords[i2]:
+                v = v+1
+                if i1 != i2:
+                    del listWords[i2]
+                    i2 = i2-1
+        if v != 0:
+            d[listWords[i1]]=v
+            del(listWords[i1])
+            i1 = i1-1
+            v = 0
+    return(d)
+
+
 train_pos = []
 train_neg = []
 test_pos = []
@@ -15,6 +37,8 @@ comment = ''
 for elt in os.listdir(path + "train/pos"):
     file = open(path + "train/pos/" + elt,'r')
     comment = file.read()
+    delPonct(comment)
+    words = comment.split()
     file.close()
     train_pos.append(comment)
 
@@ -42,3 +66,5 @@ for elt4 in os.listdir(str((path + "test/neg").encode('ascii','ignore'))[1:].rep
 #print(train_neg[50])
 
 model = Sequential()
+
+result = dict()
